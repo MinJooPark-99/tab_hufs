ee04e67c5950a39e9b666eb79944e447827af746
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-03-01 00:17:46 +0900

    Updates

---
+++ precache-manifest.cf0938fcef9ba0eb518adb903261f36f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c74a3ea0adf801f4640a",
+    "url": "/static/css/main.95c5f249.chunk.css"
+  },
+  {
+    "revision": "c74a3ea0adf801f4640a",
+    "url": "/static/js/main.c74a3ea0.chunk.js"
+  },
+  {
+    "revision": "1743bc997582ada558b9",
+    "url": "/static/js/1.1743bc99.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d7f4c3f809486db934abfc71c2750c4",
+    "url": "/static/media/logo6.8d7f4c3f.png"
+  },
+  {
+    "revision": "94b62d46c97da914863da33672f9ee44",
+    "url": "/static/media/food-basket.94b62d46.svg"
+  },
+  {
+    "revision": "9c6f28d6cd93302946cba65835b6d3ca",
+    "url": "/static/media/presentation.9c6f28d6.svg"
+  },
+  {
+    "revision": "07861617032f2934f61153cf9e68a7f2",
+    "url": "/static/media/student.07861617.svg"
+  },
+  {
+    "revision": "0c86e98e10a8a63a2d73a7fa340d08df",
+    "url": "/static/media/tent.0c86e98e.svg"
+  },
+  {
+    "revision": "84f73c4070865885609674bfe1ab9dea",
+    "url": "/static/media/swSoldier.84f73c40.png"
+  },
+  {
+    "revision": "42eb2bc7f97a636ea14794d3e6830bc6",
+    "url": "/static/media/whale.42eb2bc7.png"
+  },
+  {
+    "revision": "a6e812b5318c2dbc4fec4d9eb1088919",
+    "url": "/static/media/acm.a6e812b5.png"
+  },
+  {
+    "revision": "aac93d256c7f173231f11a0dba0a05ae",
+    "url": "/static/media/unity_logo.aac93d25.JPG"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/menu.86595150.svg"
+  },
+  {
+    "revision": "eac5d0081cb171aea0a0c5cb1c55abb2",
+    "url": "/static/media/bg_blue.eac5d008.png"
+  },
+  {
+    "revision": "13a48aa1f740f8b86eb71578cd0785b0",
+    "url": "/static/media/job.13a48aa1.png"
+  },
+  {
+    "revision": "5c5f129b1cee8f9768d566a9b8c67bcf",
+    "url": "/static/media/hackathon.5c5f129b.png"
+  },
+  {
+    "revision": "b5d3dfd19fd91503e6ecfdce1ebe7b80",
+    "url": "/static/media/gcpug.b5d3dfd1.png"
+  },
+  {
+    "revision": "fa33f3551ab22e8a7555e15a273ab7e8",
+    "url": "/static/media/hanium.fa33f355.png"
+  },
+  {
+    "revision": "b76556f58a138e00dc73684201520862",
+    "url": "/static/media/huformation.b76556f5.png"
+  },
+  {
+    "revision": "5981f6af19a251381e9b73b956f22f5f",
+    "url": "/index.html"
+  }
+];
